edc13caf2451c690a602e463227da879ea4768c6
Salma Kalife <salmakalife@hotmail.es>
2019-09-18 10:54:12 -0500

    Intento Fianl

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-var iconos = ["fa-kiss-beam", "fa-laugh-beam", "fa-grin-hearts", "fa-meh-rolling-eyes"];
+var iconos = [
+  {icono: "fa-kiss-beam", 
+  icono: "fa-laugh-beam", 
+  icono: "fa-grin-hearts",
+  icono: "fa-meh-rolling-eyes",
+  icono: "fa-hiking", 
+  icono: "fa-heart", 
+  icono: "fa-running", 
+  icono: "fa-smile"}
+];
 
 function mostrarIconoPrincipal(icono) {
   var mainIcon = document.getElementById("main_icon");
@@ -6,8 +15,30 @@ function mostrarIconoPrincipal(icono) {
   mainIcon.classList.add(icono);
 }
 
-function eliminarIcono() {
-  event.stopPropagation();
-  iconos.splice(index, 1);
-  imprimirToodsLosIconos();
+function imprimirIconos(iconos, index){
+  var lista = document.getElementById("icon_input");
+    lista.insertAdjacentHTML('beforeend',
+    `<li class="icons_list">
+    <div class="actions">
+    <a><i class="fa fa-times" onclick="eliminarIcono(`+ index +`)"></i></a>
+    `);
+}
+function imprimirTodosLosIconos() {
+  document.getElementById("icons_list").innerHTML = "";
+  iconos.forEach(imprimirIconos);
+}
+
+function agregarIcono(){
+  var icono = document.getElementById("icon_input").value;
+  iconos.push({iconos: icono});
+  imprimirTodosLosIconos(); 
+}
+
+function eliminarIcono(index){
+    iconos.splice(index, 1);
+    imprimirTodosLosIconos();
+}
+function imprimirTodosLosPendientes() {
+  document.getElementById("icon_input").innerHTML = "";
+  iconos.forEach(imprimirIconos);
 }
